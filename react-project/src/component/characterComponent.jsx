4c37c01ab91a6f12ab0e707de84b59e67a0ee165
Slaven Pjanic <slavenpjanic10@gmail.com>
2020-10-21 01:37:08 -0400

    images

--- react-project/src/component/characterComponent.jsx
+++ react-project/src/component/characterComponent.jsx
@@ -3,27 +3,30 @@ import React, {Component} from "react";
 class CharComp extends Component {
     constructor(props) {
         super(props);
+
+        let itemRef = new Array(15);
+
         this.state = {
           error: null,
           accntName: this.props.accntName,
           charName: this.props.charName,
           currentItems: [],
           inventoryImages: [
-              <img src="" id="Weapon"  alt="Weapon"/>,
-              <img src="" id="Helm"  alt="Helm"/>,
-              <img src="" id="BodyArmour"  alt="BodyArmour"/>,
-              <img src="" id="Offhand"  alt="Offhand"/>,
-              <img src="" id="Amulet"  alt="Amulet"/>,
-              <img src="" id="Ring"  alt="Left Ring"/>,
-              <img src="" id="Ring2"  alt="Right Ring"/>,
-              <img src="" id="Gloves"  alt="Gloves"/>,
-              <img src="" id="Belt"  alt="Belt"/>,
-              <img src="" id="Boots"  alt="Boots"/>,
-              <img src="" id="Flask0"  alt="First Flask"/>,
-              <img src="" id="Flask1"  alt="Second Flask"/>,
-              <img src="" id="Flask2"  alt="Third Flask"/>,
-              <img src="" id="Flask3"  alt="Fourth Flask"/>,
-              <img src="" id="Flask4"  alt="Fifth Flask"/>
+              <img src="" id="Weapon" ref={(input) => {itemRef[0] = input}} alt="Weapon"/>,
+              <img src="" id="Helm" ref={(input) => {itemRef[1] = input}} alt="Helm"/>,
+              <img src="" id="BodyArmour" ref={(input) => {itemRef[2] = input}} alt="BodyArmour"/>,
+              <img src="" id="Offhand" ref={(input) => {itemRef[3] = input}} alt="Offhand"/>,
+              <img src="" id="Amulet" ref={(input) => {itemRef[4] = input}} alt="Amulet"/>,
+              <img src="" id="Ring" ref={(input) => {itemRef[5] = input}} alt="Left Ring"/>,
+              <img src="" id="Ring2" ref={(input) => {itemRef[6] = input}} alt="Right Ring"/>,
+              <img src="" id="Gloves" ref={(input) => {itemRef[7] = input}} alt="Gloves"/>,
+              <img src="" id="Belt" ref={(input) => {itemRef[8] = input}} alt="Belt"/>,
+              <img src="" id="Boots" ref={(input) => {itemRef[9] = input}} alt="Boots"/>,
+              <img src="" id="Flask0" ref={(input) => {itemRef[10] = input}} alt="First Flask"/>,
+              <img src="" id="Flask1" ref={(input) => {itemRef[11] = input}} alt="Second Flask"/>,
+              <img src="" id="Flask2" ref={(input) => {itemRef[12] = input}} alt="Third Flask"/>,
+              <img src="" id="Flask3" ref={(input) => {itemRef[13] = input}} alt="Fourth Flask"/>,
+              <img src="" id="Flask4" ref={(input) => {itemRef[14] = input}} alt="Fifth Flask"/>
           ]
         };
       }
@@ -31,15 +34,16 @@ class CharComp extends Component {
 
 
     componentDidMount() {
-        fetch("https://www.pathofexile.com/character-window/get-items?accountName=" + accntName + "&character=" + charName)
+        console.log(this.state.accntName + "  " + this.state.charName)
+        fetch("https://www.pathofexile.com/character-window/get-items?accountName=" + this.state.accntName + "&character=" + this.state.charName)
         .then(res => res.json())
         .then(
           (result) => {
             this.setState({
               isLoaded: true,
               currentItems: result,
-              createImages();
             });
+            this.createImages();
           },
           (error) => {
             this.setState({
@@ -53,15 +57,17 @@ class CharComp extends Component {
 
     createImages = () =>{
 
-        for(let i = 0; i < this.state.currentItems.length; i++){
-            let temp  = this.state.inventoryImages.indexOf(currentItems[i].inventoryId);
-        }
+        // for(let i = 0; i < this.state.currentItems.length; i++){
+        //     let temp  = this.state.inventoryImages.indexOf(currentItems[i].inventoryId);
+        // }
 
 
     }
 
     onReturnClick = (returnCode) => {
-        this.props.returnCallBack(returnCode);
+        console.log(this.state.inventoryImages);
+        console.log(this.itemRef);
+        //this.props.returnCallBack(returnCode);
     }
 
     render(){
